5c1d0475874fc9c0dd6a224a3958695951ff45c0
Christopher Coffey <christopher.coffey@rackspace.com>
2015-08-19 14:07:42 -0500

    baseline

---
+++ scripts/ubuntu_harden.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# apt-get update/upgrade all the things
+# setup unattended upgrades
+
+# create bob user
+# add bob's SSH key
+# add bob to sudoers group
+
+# setup ntpd
+# set time to US/Central
+# set MOTD message
+
+# network hardening
+# disable IPv6
+
+# lockdown cron/at to root user
+
+# install logwatch
+
+# harden SSH configurations
+
+# IPTables rule setup ??
